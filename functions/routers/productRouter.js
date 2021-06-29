b24c1ea0f162cb02a38ad9e92590086db677b7f2
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-06-29 09:08:38 -0400

    added comments

--- functions/routers/productRouter.js
+++ functions/routers/productRouter.js
@@ -1,30 +1,37 @@
-const express = require("express");
-const Product = require("../models/productModel.js");
+const express = require('express');
+const Product = require('../models/productModel.js');
 
 const productRouter = express.Router();
 
-productRouter.get("/", async (req, res, next) => {
+productRouter.get('/', async (req, res, next) => {
     try {
+        // fetch products from db
         const products = await Product.find({});
+
+        // send back products
         res.status(200).send(products);
     } catch (error) {
-        next(error)
+        // catch error
+        next(error);
     }
-    
 });
 
-productRouter.get("/:id", async (req, res, next) => {
+productRouter.get('/:id', async (req, res, next) => {
     try {
+        // get product by id from db
         const product = await Product.findById(req.params.id);
+
+        // if product exist return product
         if (product) {
             res.json(product);
         } else {
-            res.status(404).send({message: "Product not found."});
+            // else return error
+            res.status(404).send({ message: 'Product not found.' });
         }
     } catch (error) {
-        next(error)
+        // catch error
+        next(error);
     }
-    
 });
 
 module.exports = productRouter;
